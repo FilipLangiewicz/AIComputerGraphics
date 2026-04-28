40151e384383e4f27d2745b38daba5660ddb8fc7
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-04-28 02:15:24 +0200

    Add SelfAttention

--- project3-rendering/diffusion_model/unet.py
+++ project3-rendering/diffusion_model/unet.py
@@ -32,17 +32,36 @@ class ResBlock(nn.Module):
         self.norm2 = nn.GroupNorm(8, out_ch)
 
         self.time_proj = nn.Linear(time_emb_dim, out_ch)
-        self.cond_proj = nn.Linear(cond_dim, out_ch)
+        self.cond_proj = nn.Linear(cond_dim, out_ch * 2)
 
         self.skip = nn.Conv2d(in_ch, out_ch, 1) if in_ch != out_ch else nn.Identity()
 
     def forward(self, x, t_emb, cond):
         h = self.norm1(F.silu(self.conv1(x)))
         h = h + self.time_proj(F.silu(t_emb))[:, :, None, None]
-        h = h + self.cond_proj(F.silu(cond))[:, :, None, None]
+
+        scale, shift = self.cond_proj(F.silu(cond)).chunk(2, dim=1)
+
+        h = h * (1 + scale[:, :, None, None]) + shift[:, :, None, None]
         h = self.norm2(F.silu(self.conv2(h)))
 
         return h + self.skip(x)
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, ch: int, heads: int = 4):
+        super().__init__()
+
+        self.norm = nn.GroupNorm(8, ch)
+        self.attn = nn.MultiheadAttention(ch, heads, batch_first=True)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+
+        h = self.norm(x).reshape(B, C, H*W).permute(0, 2, 1)
+        h, _ = self.attn(h, h, h)
+
+        return x + h.permute(0, 2, 1).reshape(B, C, H, W)
 
 
 class UNet(nn.Module):
@@ -65,13 +84,15 @@ class UNet(nn.Module):
         c = base_ch
 
         # Encoder
-        self.enc1 = ResBlock(3,     c,   time_emb_dim, time_emb_dim)   # 128
-        self.enc2 = ResBlock(c,   c*2,   time_emb_dim, time_emb_dim)   # 64
-        self.enc3 = ResBlock(c*2, c*4,   time_emb_dim, time_emb_dim)   # 32
+        self.enc1 = ResBlock(3,     c,   time_emb_dim, time_emb_dim)
+        self.enc2 = ResBlock(c,   c*2,   time_emb_dim, time_emb_dim)
+        self.enc3 = ResBlock(c*2, c*4,   time_emb_dim, time_emb_dim)
         self.down = nn.MaxPool2d(2)
 
         # Bottleneck
-        self.mid = ResBlock(c*4, c*4, time_emb_dim, time_emb_dim)
+        self.mid1 = ResBlock(c * 4, c * 4, time_emb_dim, time_emb_dim)
+        self.mid_attn = SelfAttention(c * 4)
+        self.mid2 = ResBlock(c * 4, c * 4, time_emb_dim, time_emb_dim)
 
         # Decoder
         self.up3  = nn.ConvTranspose2d(c*4, c*4, 2, 2)
@@ -87,11 +108,13 @@ class UNet(nn.Module):
         t_emb = self.time_emb(t)
         c_emb = self.cond_emb(cond)
 
-        e1 = self.enc1(x, t_emb, c_emb)                    # (B, c, 128, 128)
-        e2 = self.enc2(self.down(e1), t_emb, c_emb)        # (B, 2c, 64, 64)
-        e3 = self.enc3(self.down(e2), t_emb, c_emb)        # (B, 4c, 32, 32)
+        e1 = self.enc1(x, t_emb, c_emb)
+        e2 = self.enc2(self.down(e1), t_emb, c_emb)
+        e3 = self.enc3(self.down(e2), t_emb, c_emb)
 
-        m = self.mid(self.down(e3), t_emb, c_emb)         # (B, 4c, 16, 16)
+        m = self.mid1(self.down(e3), t_emb, c_emb)
+        m = self.mid_attn(m)
+        m = self.mid2(m, t_emb, c_emb)
 
         d3 = self.dec3(torch.cat([self.up3(m), e3], 1), t_emb, c_emb)
         d2 = self.dec2(torch.cat([self.up2(d3), e2], 1), t_emb, c_emb)
